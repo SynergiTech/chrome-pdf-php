078bb703c5834bf30ef21fa0b8ef71499af83d6f
Will Power <1619102+willpower232@users.noreply.github.com>
2025-05-13 10:59:30 +0100

    ✨ more enums

--- src/Browserless/EndpointsEnum.php
+++ src/Browserless/EndpointsEnum.php
@@ -3,8 +3,17 @@
 namespace SynergiTech\ChromePDF\Browserless;
 
 enum EndpointsEnum: string
-{
+{    
     case Default = 'https://chrome.browserless.io';
+
+    // V1 and V2
+    case ChromeEuAms = 'https://chrome-eu-ams.browserless.io';
+    case ChromeEuUk = 'https://chrome-eu-uk.browserless.io';
+    case ChromeUsEast = 'https://chrome-us-east.browserless.io';
+    case ChromeUsWest = 'https://chrome-us-west.browserless.io';
+
+    // V2 only
+    case Amsterdam = 'https://production-ams.browserless.io';
     case London = 'https://production-lon.browserless.io';
     case SanFrancisco = 'https://production-sfo.browserless.io';
 }
